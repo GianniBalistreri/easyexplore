55b49365b6650142576ce7c9d9e1da6e92117e0e
GianniBalistreri <gbalistreri@gmx.de>
2021-03-08 14:30:17 +0100

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [r.strip() for r in requires.split('\n') if ((r.strip()[0] != "#") an
 
 setuptools.setup(
     name='easyexplore',
-    version='0.6.4',
+    version='0.6.5',
     author='Gianni Francesco Balistreri',
     author_email='gbalistreri@gmx.de',
     description='Toolbox for easy and effective data exploration',
